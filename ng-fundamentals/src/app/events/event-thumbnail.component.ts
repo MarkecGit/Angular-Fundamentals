8d5ca5210c671ab3d3309adc895fd2f6c162ca71
Marko <marko.kralik@3mi-lab.hr>
2019-12-28 12:53:52 +0100

    Angular template syntax 3 - Hiding Content with the [Hidden] Binding

--- ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -9,11 +9,11 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
         <div>Date: {{ event.date }}</div>
         <div>{{ event.time }}</div>
         <div>\${{ event.price }}</div>
-        <div *ngIf="event?.location">
-            <span>Location: {{ event.location.address }}</span>
-            <span class="pad-left">{{ event.location.city }}, {{ event.location.country }}</span>
+        <div [hidden] = "!event?.location">
+            <span>Location: {{ event?.location?.address }}</span>
+            <span class="pad-left">{{ event?.location?.city }}, {{ event?.location?.country }}</span>
         </div>
-        <div *ngIf="event?.onlineUrl">
+        <div [hidden] = "!event?.onlineUrl">
             Online URL: {{ event?.onlineUrl }}
         </div>
     </div>
