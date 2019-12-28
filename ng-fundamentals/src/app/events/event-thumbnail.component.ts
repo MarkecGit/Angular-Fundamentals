1c5f2df7360b1d320042dce40ad9fe3c7cc275ac
Marko <marko.kralik@3mi-lab.hr>
2019-12-28 19:37:58 +0100

    Angular template syntax 7 - Styling components with ngStyle

--- ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     <div class="well hoverwell thumbnail">
         <h2>{{ event?.name }}</h2>
         <div>Date: {{ event.date }}</div>
-        <div [ngClass]="getStatTimeClass()"
+        <div [ngStyle]="getStartTimeStyle()"
         [ngSwitch]="event?.time">
             Time: {{ event?.time }}
             <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
@@ -25,8 +25,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     </div>
     `,
     styles:  [`
-        .green { color: #003300 !important; }
-        .bold { font-weight: bold; }
+        //.green { color: #003300 !important; }
+        //.bold { font-weight: bold; }
         .thumbnail { min-height: 210px; },
         .pad-left { margin-left: 10px; },
         .well div { color: #bbb;}          
@@ -50,7 +50,14 @@ export class EventThumbnailComponent {
          if (this.event && this.event.time === '8:00 am') 
             return ['green', 'bold'];
          return [];
-
     }
+
+    getStartTimeStyle():any {
+        if (this.event && this.event.time === '8:00 am') 
+            return { color: '#003300', 'font-weight': 'bold' };
+         return {};
+    }
+
+
 
 }
