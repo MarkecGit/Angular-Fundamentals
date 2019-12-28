28e9916e48d13ec24c1569ab583d9ae162bb3c4b
Marko <marko.kralik@3mi-lab.hr>
2019-12-28 12:56:48 +0100

    Angular template syntax 4 - Cleaning back to ngIf

--- ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -9,11 +9,11 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
         <div>Date: {{ event.date }}</div>
         <div>{{ event.time }}</div>
         <div>\${{ event.price }}</div>
-        <div [hidden] = "!event?.location">
+        <div *ngIf="event?.location">
             <span>Location: {{ event?.location?.address }}</span>
             <span class="pad-left">{{ event?.location?.city }}, {{ event?.location?.country }}</span>
         </div>
-        <div [hidden] = "!event?.onlineUrl">
+        <div *ngIf="event?.onlineUrl">
             Online URL: {{ event?.onlineUrl }}
         </div>
     </div>
