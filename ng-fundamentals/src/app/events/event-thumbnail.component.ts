613312e55cd122007650262e7161f421b17600eb
Marko <marko.kralik@3mi-lab.hr>
2019-12-28 13:54:54 +0100

    Angular template syntax 5 - ngSwitch

--- ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -7,8 +7,13 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     <div class="well hoverwell thumbnail">
         <h2>{{ event?.name }}</h2>
         <div>Date: {{ event.date }}</div>
-        <div>{{ event.time }}</div>
-        <div>\${{ event.price }}</div>
+        <div [ngSwitch]="event?.time">
+            Time: {{ event?.time }}
+            <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+            <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+            <span *ngSwitchDefault>(Normal Start)</span>
+        </div>      
+        <div>Price: \${{ event.price }}</div>
         <div *ngIf="event?.location">
             <span>Location: {{ event?.location?.address }}</span>
             <span class="pad-left">{{ event?.location?.city }}, {{ event?.location?.country }}</span>
