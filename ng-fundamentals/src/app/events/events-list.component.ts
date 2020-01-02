12bfcf5a8d7200b8a27122196df2c10fa39e10a6
Marko <marko.kralik@gmail.com>
2020-01-02 21:00:09 +0100

    Routing and Navigating Pages 8 - Pre-loading Data for Components, Resolver

--- ng-fundamentals/src/app/events/events-list.component.ts
+++ ng-fundamentals/src/app/events/events-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
 import { ToastrService } from '../common/toastr.service';
+import { ActivatedRoute } from '@angular/router';
+
 declare let toastr;
 
 @Component({   
@@ -20,16 +22,21 @@ declare let toastr;
 })
 
 export class EventsListComponent implements OnInit {
-  events: any[];
+  //events: any[];
+  events: any;
 
   constructor (private eventService: EventService,
-               private toastr: ToastrService) {  
-
-    
+               private toastr: ToastrService,
+               private route: ActivatedRoute) {  
+   
   }
 
   ngOnInit() {
-    this.events = this.eventService.getEvents();
+    //this.events = this.eventService.getEvents();
+    // this.eventService.getEvents().subscribe(
+    //   events => { this.events = events }
+    // );
+    this.events = this.route.snapshot.data['events'];
   }
 
   handleThumbnailClick(eventName) {
