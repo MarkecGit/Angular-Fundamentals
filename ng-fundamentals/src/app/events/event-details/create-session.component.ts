5d1d1fe8586284ec185bd691287722972aa2b47c
Marko <marko.kralik@gmail.com>
2020-01-13 19:41:07 +0100

    Communicating between components 2 - Passing Data out of a Child Component

--- ng-fundamentals/src/app/events/event-details/create-session.component.ts
+++ ng-fundamentals/src/app/events/event-details/create-session.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
 import { ISession, restrictedWords } from '../shared';
 
 @Component({
+  selector: 'create-session',
   templateUrl: './create-session.component.html',
   styles: [`
   em {float: right; color: #E05C65; padding-left: 10px;}
@@ -15,7 +16,9 @@ import { ISession, restrictedWords } from '../shared';
   `]
 })
 export class CreateSessionComponent implements OnInit {
-
+  @Output() saveNewSession = new EventEmitter();
+  @Output() cancelAddSession = new EventEmitter();
+  
   newSessionForm: FormGroup;
   name: FormControl;
   presenter: FormControl;
@@ -53,6 +56,11 @@ export class CreateSessionComponent implements OnInit {
         abstract: formValues.abstract,
         voters: []
     }
-    console.log(session);
+    this.saveNewSession.emit(session);
+    //console.log(session);
+  }
+
+  cancel() {
+    this.cancelAddSession.emit();
   }
 }
