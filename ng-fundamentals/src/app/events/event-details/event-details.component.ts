3268a1bf418bb45832cb0211d1d76b86786e9c8d
Marko <marko.kralik@3mi-lab.hr>
2020-01-16 13:29:56 +0100

    Displaying Data with Pipes 3 - Creating a Filtering Display

--- ng-fundamentals/src/app/events/event-details/event-details.component.ts
+++ ng-fundamentals/src/app/events/event-details/event-details.component.ts
@@ -15,6 +15,7 @@ import { IEvent, ISession } from '../shared';
 export class EventDetailsComponent {
     event: IEvent;
     addMode: boolean;
+    filerBy: string = 'all';
 
     constructor(private eventService: EventService,
         private route: ActivatedRoute) {
