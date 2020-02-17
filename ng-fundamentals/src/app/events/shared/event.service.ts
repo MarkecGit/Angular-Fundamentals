74e6d198ea88919da7b3932b775a8461904fb4c7
Marko <marko.kralik@3mi-lab.hr>
2020-02-17 16:21:02 +0100

    Communicating with the Server Using HTTP, Observables, and Rx 4 - Using QueryString Parameters

--- ng-fundamentals/src/app/events/shared/event.service.ts
+++ ng-fundamentals/src/app/events/shared/event.service.ts
@@ -42,25 +42,27 @@ export class EventService {
     //   EVENTS[index] = event;
     // }
 
-    searchSessions(searchTerm: string) {
-      var term = searchTerm.toLocaleLowerCase();
-      var results: ISession[] = [];
+    searchSessions(searchTerm: string): Observable<ISession[]> {
+      return this.http.get<ISession[]>('/api/sessions/search?search=' + searchTerm)
+        .pipe(catchError(this.handleError<ISession[]>('searchSessions')));
+      // var term = searchTerm.toLocaleLowerCase();
+      // var results: ISession[] = [];
 
-      EVENTS.forEach(event => {
-        var matchingSessions = event.sessions.filter( session =>
-          session.name.toLocaleLowerCase().indexOf(term) > -1);
-        matchingSessions = matchingSessions.map((session:any) => {
-          session.eventId = event.id;
-          return session;
-        })
-        results = results.concat(matchingSessions);
-      });
+      // EVENTS.forEach(event => {
+      //   var matchingSessions = event.sessions.filter( session =>
+      //     session.name.toLocaleLowerCase().indexOf(term) > -1);
+      //   matchingSessions = matchingSessions.map((session:any) => {
+      //     session.eventId = event.id;
+      //     return session;
+      //   })
+      //   results = results.concat(matchingSessions);
+      // });
 
-      var emitter = new EventEmitter(true);
-      setTimeout(() => {
-        emitter.emit(results);
-      }, 100);
-      return emitter;
+      // var emitter = new EventEmitter(true);
+      // setTimeout(() => {
+      //   emitter.emit(results);
+      // }, 100);
+      // return emitter;
     }
 
     private handleError<T> (operation = 'operation', result?:  T) {
