84e68bfc54b30a02369a5ebbcb80efa66f6ff066
Marko <marko.kralik@3mi-lab.hr>
2020-03-04 16:02:46 +0100

    Testing Angular Components with Integrated Tests 2 - Testing Components with Deep Integrated Tests

--- ng-fundamentals/src/app/events/event-details/session-list.component.integrated.spec.ts
+++ ng-fundamentals/src/app/events/event-details/session-list.component.integrated.spec.ts
@@ -3,6 +3,9 @@ import { SessionListComponent } from './session-list.component'
 import { DebugElement } from '@angular/core'
 import { AuthService } from 'src/app/user/auth.service'
 import { VoterService } from '.'
+import { UpvoteComponent } from './upvote.component'
+import { DurationPipe } from '../shared'
+import { CollapsibleWellComponent } from 'src/app/common'
 
 
 describe('SessionListComponent', () => {
@@ -18,7 +21,10 @@ describe('SessionListComponent', () => {
         TestBed.configureTestingModule({
            imports: [],
            declarations: [
-               SessionListComponent
+               SessionListComponent,
+               UpvoteComponent,
+               CollapsibleWellComponent,
+               DurationPipe
            ],
            providers: [
                { provide: AuthService, useValue: mockAuthService },
@@ -35,7 +41,28 @@ describe('SessionListComponent', () => {
         component = fixture.componentInstance;
         debugEl = fixture.debugElement;
         element = fixture.nativeElement;
+    })
 
+    describe('initial display', () => {
+
+        it('should have the correct session title', () => {
+            component.sessions = [{ id: 3, name: 'Session 1',
+                presenter: 'Joe', duration: 1, level: 'beginner',
+                abstract: 'abstract', voters: ['john', 'bob']}];
+        
+            component.filterBy = 'all';
+            component.sortBy = 'name';
+            component.eventId = 4;
+
+            component.ngOnChanges();
+            fixture.detectChanges();
+
+            expect(element.querySelector('[well-title]').textContent)
+                .toContain('Session 1');
+
+            
+
+        })
     })
 })
 
